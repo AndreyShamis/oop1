7cab4e2cb203a3a1a2376ec927941b9499d2078b
lolnik@gmail.com <lolnik@gmail.com@915c7e4a-0830-d67a-7742-9bd6d4e428b7>
2010-12-18 21:19:38 +0000

    thats it

--- ex4/ex4-321470882_309480051/file.cpp
+++ ex4/ex4-321470882_309480051/file.cpp
@@ -1,9 +1,30 @@
 #include "file.h"
 
+//=============================================================================
+bool file::createDB()
+{ 
+	ofstream myfile;
+	myfile.open (DEFAULT_DB_NAME,ios::app);
+	if(myfile.is_open())
+	{
+		myfile.close();
+		return(true);
+	}	
+	return (false);
+
+}
+
+void file::CloseFile(ifstream &myReadFile)
+{
+	myReadFile.close();
+}
 file::file(void)
 {
+	;
 }
 
 file::~file(void)
 {
+	;
 }
+
